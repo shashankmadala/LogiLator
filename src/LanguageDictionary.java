05e12ed368bb8709af58b68e3c08f2ccdc1d81e6
Shashank Madala <madala.shashank@gmail.com>
2026-05-26 09:43:18 -0400

    language dictionary. arraylist

--- src/LanguageDictionary.java
+++ src/LanguageDictionary.java
@@ -1,3 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// all the word pairs for one language
 public class LanguageDictionary {
 
+    private ArrayList<DictionaryEntry> entries;
+
+    public LanguageDictionary() {
+        entries = new ArrayList<DictionaryEntry>();
+    }
+
+    public String translateWord(String word) {
+        for (int i = 0; i < entries.size(); i++) {
+            DictionaryEntry current = entries.get(i);
+
+            if (current.matches(word)) {
+                return current.getTranslatedWord();
+            }
+        }
+        return null;
+    }
+
+    public void loadFromFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scan = new Scanner(file);
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+
+            if (line.length() == 0) {
+                continue;
+            }
+
+            int eq = line.indexOf("=");
+            if (eq == -1) {
+                continue;
+            }
+
+            String english = line.substring(0, eq);
+            String translated = line.substring(eq + 1);
+
+            entries.add(new DictionaryEntry(english, translated));
+        }
+
+        scan.close();
+    }
 }
