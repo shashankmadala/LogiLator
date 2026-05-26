9591a3da12b22794f57988e7d382cd6ad2474ebf
Akhil Kohli <theakhilkohli@gmail.com>
2026-05-26 09:39:12 -0400

    updated spanish dict

--- src/Translator.java
+++ src/Translator.java
@@ -1,3 +1,20 @@
 public class Translator {
-    
+    private String language;
+    private String phrase; 
+    private String word;
+
+    public Translator(String language, String phrase, String word){
+        this.language = language;
+        this.phrase = phrase;
+        this.word = word;
+    }
+    public String getLanguage(){
+        return language;
+    }
+    public String getPhrase(){
+        return phrase;
+    }
+    public String getWord(){
+        return word;
+    }
 }
