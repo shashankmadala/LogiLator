13bc638ad2e7c1e18b0d8c6a014809e3f094b1d1
Shashank Madala <madala.shashank@gmail.com>
2026-05-26 09:25:59 -0400

    dictionary entry constructor and main methods

--- src/DictionaryEntry.java
+++ src/DictionaryEntry.java
@@ -1,3 +1,23 @@
 public class DictionaryEntry {
+    private String englishWord;
+    private String translatedWord;
 
+    // set up a new entry. this is when we read a line from the file
+    public DictionaryEntry(String englishWord, String translatedWord) {
+        this.englishWord = englishWord;
+        this.translatedWord = translatedWord;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public String getTranslatedWord() {
+        return translatedWord;
+    }
+
+    // check if this entry is the word we're looking for. we learned equalsIgnoreCase, we searched up how to use it properly.
+    public boolean matches(String word) {
+        return englishWord.equalsIgnoreCase(word);
+    }
 }
